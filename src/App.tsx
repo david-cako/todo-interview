cc453f7fca64f50a83273458452ebb1a7c7e7450
David Cako <dc@cako.io>
2022-10-24 10:04:26 -0600

    client side drag implementation

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const apiClient = new ApiClient();
 function App() {
   const [todos, setTodos] = useState<ToDo[]>([]);
   const [label, setLabel] = useState('');
+  const [isDragging, setIsDragging] = useState(false);
 
   useEffect(() => {
     apiClient
@@ -36,8 +37,43 @@ function App() {
     setTodos(newTodos);
   }
 
+  const onMove = (todo: ToDo, { y }: { y: number }) => {
+    let newIdx;
+
+    const items = document.querySelectorAll(".todo-item");
+
+    // find new item index
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+
+      const itemId = item.getAttribute("data-item-id");
+
+      // exclude currently dragged item
+      if (itemId !== null && todo.id === parseInt(itemId)) {
+        continue;
+      }
+
+      const rect = item.getBoundingClientRect();
+      if (y <= rect.bottom) {
+        newIdx = i;
+        break;
+      }
+    }
+
+    if (newIdx !== undefined) {
+      const newTodos = todos.filter(item => item.id !== todo.id);
+
+      newTodos.splice(newIdx, 0, todo);
+
+      setTodos(newTodos);
+    }
+  }
+
   return (
-    <>
+    <div style={isDragging
+      ? { userSelect: "none", WebkitUserSelect: "none" }
+      : undefined
+    }>
       <h1>To Do List</h1>
 
       <AddTodo label={label}
@@ -49,9 +85,12 @@ function App() {
         <TodoItem todo={todo}
           key={todo.id}
           onMarkDoneClick={onMarkDoneClick}
+          onDragStart={() => setIsDragging(true)}
+          onDragEnd={() => setIsDragging(false)}
+          onMove={onMove}
         />
       ))}
-    </>
+    </div>
   );
 }
 
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,13 +1,58 @@
+import React, { useState } from 'react';
 import { ToDo } from '../ApiClient';
 
 interface TodoItemProps {
     todo: ToDo;
     onMarkDoneClick: (id: number) => any;
+    onDragStart: () => any;
+    onDragEnd: () => any;
+    onMove: (todo: ToDo, coords: { y: number }) => any;
 }
 
-export default function TodoItem({ todo, onMarkDoneClick }: TodoItemProps) {
+export default function TodoItem({
+    todo,
+    onMarkDoneClick,
+    onDragStart,
+    onDragEnd,
+    onMove
+}: TodoItemProps) {
+    const [isDragging, setIsDragging] = useState(false);
+    const [yCoord, setYCoord] = useState(0);
+
+    const onMouseMove = (e: MouseEvent) => {
+        setYCoord(e.clientY);
+    }
+
+    const onMouseDown = (e: React.MouseEvent) => {
+        setIsDragging(true);
+
+        onDragStart();
+        document.addEventListener("mousemove", onMouseMove);
+    }
+
+    const onMouseUp = (e: React.MouseEvent) => {
+        setIsDragging(false);
+
+        onDragEnd();
+        onMove(todo, { y: e.clientY });
+
+        document.removeEventListener("mousemove", onMouseMove);
+    }
+
     return (
-        <div className="todo-item">
+        <div className="todo-item"
+            data-item-id={todo.id}
+            onMouseDown={onMouseDown}
+            onMouseUp={onMouseUp}
+            style={isDragging ?
+                {
+                    position: "absolute",
+                    top: yCoord,
+                    width: 500
+                }
+                : undefined
+            }
+        >
             <label
                 style={{ textDecoration: todo.done ? 'line-through' : 'none' }}
             >
